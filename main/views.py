7c6060f2e3dbe344355e8aa2a8b18c4ddeb4de85
Sasha <html63@mail.ru>
2022-02-17 06:01:54 +0300

    Edits

--- main/views.py
+++ main/views.py
@@ -12,29 +12,28 @@ def home(request):
 
 def users_table(request):
     GET = request.GET
-    user_status = {}
-    for i in Account.objects.all().values_list('data_id', flat=True):
-        user_status[User.objects.get(id=i)] = Account.objects.get(data_id=i).status
-        print(Account.objects.get(data_id=i).status)
-    context = {
-    'user_status' : user_status,
-    }
 
     if ('action' in GET) and ('id_user' in GET):
         GET_list_id_user = GET.getlist('id_user')
         if (GET['action'] == 'Block'):
             for i in GET_list_id_user:
-                d = User.objects.get(id=i)
-                print("BLOCK USER:  %s" %d)
-
+                d = Account.objects.get(data_id=i)
+                d.set_status('Block')
+                print("BLOCK USER:  %s" %d.status)
         if (GET['action'] == 'Unblock'):
             for i in GET_list_id_user:
-                d = User.objects.get(id=i)
-                print("UNBLOCK USER:  %s" %d)
-
+                d = Account.objects.get(data_id=i)
+                d.set_status('Unblock')
+                print("UNBLOCK USER:  %s" %d.status)
         if (GET['action'] == 'Delete' and GET['id_user']):
             for i in GET_list_id_user:
-                d = User.objects.get(id=i)
+                d = Account.objects.get(data_id=i)
                 d.delete()
 
+    user_status = {}
+    for i in Account.objects.all().values_list('data_id', flat=True):
+        user_status[User.objects.get(id=i)] = Account.objects.get(data_id=i).status
+    context = {
+    'user_status' : user_status,
+    }
     return render(request, 'main/users_table.html', { 'context':user_status} )
